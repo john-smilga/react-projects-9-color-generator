7ce2e3e56a5c9751b433954a90be82143516d977
john-smilga <johnsmilga@gmail.com>
2020-10-03 19:14:56 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('');
+  const [list, setList] = useState(new Values('#f15025').all(10));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+      setList(colors);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <section className='container'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+          />
+          <button type='submit'>submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
