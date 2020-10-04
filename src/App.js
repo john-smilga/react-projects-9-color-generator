cda1593b434a762085db7bbe527eb31db32a807b
john-smilga <johnsmilga@gmail.com>
2020-10-03 19:41:39 -0700

    color generator ready

--- src/App.js
+++ src/App.js
@@ -5,28 +5,34 @@ import Values from 'values.js';
 
 function App() {
   const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
   const [list, setList] = useState(new Values('#f15025').all(10));
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      console.log(colors);
       setList(colors);
+      setError(false);
     } catch (error) {
+      setError(true);
       console.log(error);
     }
   };
   return (
     <>
       <section className='container'>
+        <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
           <input
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
           />
-          <button type='submit'>submit</button>
+          <button type='submit' className='btn'>
+            submit
+          </button>
         </form>
       </section>
       <section className='colors'>
